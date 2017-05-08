f25dcb2a2c8d3a24fdcd09d1698081629555cced
Christoph Glaubitz <christoph.glaubitz@innovo-cloud.de>
2017-05-08 10:30:49 +0200

    Changed Copyright to 2017

--- netbox/client.go
+++ netbox/client.go
@@ -1,4 +1,4 @@
-// Copyright 2016 The go-netbox Authors.
+// Copyright 2017 The go-netbox Authors.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
