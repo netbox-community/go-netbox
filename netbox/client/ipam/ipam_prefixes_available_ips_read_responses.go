816e5a285e857f54e1334f4d6604582f6ff579e5
Stefan McShane <stefan.mcshane@options-it.com>
2020-05-04 13:20:28 -0400

    Updated availble-ips swagger. Issue raised in netbox #4571

--- netbox/client/ipam/ipam_prefixes_available_ips_read_responses.go
+++ netbox/client/ipam/ipam_prefixes_available_ips_read_responses.go
@@ -61,23 +61,21 @@ func NewIpamPrefixesAvailableIpsReadOK() *IpamPrefixesAvailableIpsReadOK {
 IpamPrefixesAvailableIpsReadOK ipam prefixes available ips read o k
 */
 type IpamPrefixesAvailableIpsReadOK struct {
-	Payload *models.Prefix
+	Payload []*models.NestedIPAddress
 }
 
 func (o *IpamPrefixesAvailableIpsReadOK) Error() string {
 	return fmt.Sprintf("[GET /ipam/prefixes/{id}/available-ips/][%d] ipamPrefixesAvailableIpsReadOK  %+v", 200, o.Payload)
 }
 
-func (o *IpamPrefixesAvailableIpsReadOK) GetPayload() *models.Prefix {
+func (o *IpamPrefixesAvailableIpsReadOK) GetPayload() []*models.NestedIPAddress {
 	return o.Payload
 }
 
 func (o *IpamPrefixesAvailableIpsReadOK) readResponse(response runtime.ClientResponse, consumer runtime.Consumer, formats strfmt.Registry) error {
 
-	o.Payload = new(models.Prefix)
-
 	// response payload
-	if err := consumer.Consume(response.Body(), o.Payload); err != nil && err != io.EOF {
+	if err := consumer.Consume(response.Body(), &o.Payload); err != nil && err != io.EOF {
 		return err
 	}
 
