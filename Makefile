1363e14cfc7bce4bd3d5ee93c09ca70543c51279
Fabian Breckle <fabian.breckle@breuninger.de>
2020-10-28 10:40:00 +0100

    Update to 2.9
    - Add fork changes to README.md
    - Add another workaround for json marshalling error
    - Re-add preprocession

--- Makefile
+++ Makefile
@@ -1,9 +1,9 @@
-deps:
-	go mod download
-	GO111MODULE=off go get -u github.com/myitcv/gobin
+swagger=docker run --rm -it -e GOPATH=$(HOME)/go:/go -v $(HOME):$(HOME) -w $$(pwd) quay.io/goswagger/swagger:v0.25.0
+generate:
+	$(swagger) generate client --target=./netbox --spec=./swagger.processed.json --copyright-file=./copyright_header.txt
 
-generate: deps
-	gobin -m -run github.com/go-swagger/go-swagger/cmd/swagger generate client --copyright-file=copyright_header.txt
+preprocess:
+	bash preprocess.sh swagger.json > swagger.processed.json
 
 clean:
 	rm -rf netbox/client netbox/models
---
+++ preprocess.sh
@@ -0,0 +1,5 @@
+### Set omitempty=false for the optional properties of the WritableVirtualMachineWithConfigContext object
+
+# with_entries(foo) is a shorthand for to_entries | map(foo) | from_entries per https://stedolan.github.io/jq/manual/#to_entries,from_entries,with_entries
+# jq '.definitions.WritableVirtualMachineWithConfigContext.properties = (.definitions.WritableVirtualMachineWithConfigContext.properties | to_entries | map(if .value."x-nullable" then .value."x-omitempty"=false else . end) | from_entries)' swagger.json > bla.json
+jq '.definitions.WritableVirtualMachineWithConfigContext.properties = (.definitions.WritableVirtualMachineWithConfigContext.properties | with_entries(if .value."x-nullable" then .value."x-omitempty"=false else . end))' $1
