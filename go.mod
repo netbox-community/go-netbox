cc909b7da3ec433a290ce15cfd3a39e4d8425f88
Víctor Díaz Marco <victor@diazmarco.me>
2023-10-17 19:31:21 +0200

    Add client helper.

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/netbox-community/go-netbox/v3
 go 1.21
 
 require (
+	github.com/deepmap/oapi-codegen v1.15.0
 	github.com/oapi-codegen/runtime v1.0.0
 	gopkg.in/yaml.v2 v2.4.0
 )
--- main.go
+++ main.go
@@ -2,3 +2,29 @@ package netbox
 
 //go:generate oapi-codegen -package netbox -generate types -o types.go api/openapi.yaml
 //go:generate oapi-codegen -package netbox -generate client -o client.go api/openapi.yaml
+
+import (
+	"fmt"
+
+	"github.com/deepmap/oapi-codegen/pkg/securityprovider"
+)
+
+const authHeaderName = "Authorization"
+const authHeaderFormat = "Token %v"
+
+func NewClientWithToken(host string, token string) (*ClientWithResponses, error) {
+	provider, err := securityprovider.NewSecurityProviderApiKey(
+		"header",
+		authHeaderName,
+		fmt.Sprintf(authHeaderFormat, token),
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return NewClientWithResponses(
+		host,
+		WithRequestEditorFn(provider.Intercept),
+	)
+}
