e3519984b52f5b13b26047247e956049e54cfcb4
user.email <anshulkam@gmail.com>
2026-01-12 21:15:27 -0500

    fixup! feat(api): upgrade spec for netbox 4.5

--- main.go
+++ main.go
@@ -1,8 +1,16 @@
 package netbox
 
+import (
+	"fmt"
+	"strings"
+)
+
 const authHeaderName = "Authorization"
 const languageHeaderName = "Accept-Language"
 const languageHeaderValue = "en-US"
+const v1HeaderFormat = "Token %s"
+const v2HeaderFormat = "Bearer %s"
+const v2Prefix = "nbt_"
 
 func NewAPIClientFor(host string, token string) *APIClient {
 	cfg := NewConfiguration()
@@ -20,4 +28,14 @@ func NewAPIClientFor(host string, token string) *APIClient {
 	)
 
 	return NewAPIClient(cfg)
+}
+
+func GetFormattedAPIToken(token string) string {
+	var authHeaderFormat string
+	if strings.HasPrefix(token, v2Prefix) {
+		authHeaderFormat = v2HeaderFormat
+	} else {
+		authHeaderFormat = v1HeaderFormat
+	}
+	return fmt.Sprintf(authHeaderFormat, token)
 }
